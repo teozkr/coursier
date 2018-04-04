4fd681a7f9b21a04dfe0f0597d6436822f51ba37
Alexandre Archambault <alexandre.archambault@gmail.com>
2018-04-04 11:33:13 +0200

    Cross-compile for 2.13.0-M3

--- project/ScalaVersion.scala
+++ project/ScalaVersion.scala
@@ -1,11 +1,12 @@
 
 object ScalaVersion {
 
+  def scala213 = "2.13.0-M3"
   def scala212 = "2.12.5"
   def scala211 = "2.11.12"
   def scala210 = "2.10.7"
 
-  val versions = Seq(scala212, scala211, scala210)
+  val versions = Seq(scala213, scala212, scala211, scala210)
 
   val map = versions
     .map { v =>
